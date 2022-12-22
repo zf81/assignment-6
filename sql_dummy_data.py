4a72ae59c9169f7e75ea571e52f45f2d28f71eb5
zf81 <111600840+zf81@users.noreply.github.com>
2022-12-22 15:10:58 -0500

    update/

--- sql_dummy_data.py
+++ sql_dummy_data.py
@@ -1,52 +1,54 @@
-# Import packages
+#import packages
 import dbm
 import pandas as pd 
 import sqlalchemy
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
-from faker import Faker
+from faker import Faker # https://faker.readthedocs.io/en/master/
 import uuid
 import random
+import cryptography
 
+#login credentials
 load_dotenv()
+
 AZURE_MYSQL_HOSTNAME = os.getenv("AZURE_MYSQL_HOSTNAME")
 AZURE_MYSQL_USER = os.getenv("AZURE_MYSQL_USERNAME")
-AZURE_MYSQL_PASSWORD = os.getenv("AZURE_MYSQL_PASSWORD")
+AZURE_MYSQL_PASSWORD= os.getenv("AZURE_MYSQL_PASSWORD")
 AZURE_MYSQL_DATABASE = os.getenv("AZURE_MYSQL_DATABASE")
 
+#connecting
 connection_string_azure = f'mysql+pymysql://{AZURE_MYSQL_USER}:{AZURE_MYSQL_PASSWORD}@{AZURE_MYSQL_HOSTNAME}:3306/{AZURE_MYSQL_DATABASE}'
 db_azure = create_engine(connection_string_azure)
 
-### show dbs
-db_azure.table_names() 
+
+### show databases
 print(db_azure.table_names())
 
-# Insert Fake Patient Data 
-fake = Faker ()
+#### fake stuff 
+fake = Faker()
+
 fake_patients = [
     {
+        #keep just the first 8 characters of the uuid
         'mrn': str(uuid.uuid4())[:8], 
         'first_name':fake.first_name(), 
         'last_name':fake.last_name(),
-        'zip_code':fake.zipcode(),
         'dob':(fake.date_between(start_date='-90y', end_date='-20y')).strftime("%Y-%m-%d"),
         'gender': fake.random_element(elements=('M', 'F')),
-        'contact_mobile':fake.phone_number(),
-        'contact_home':fake.phone_number()
-    } for x in range(15)]
+        'zip_code':fake.zipcode(),
+        'city':fake.city(), 
+        'state':fake.state(),
+        'phone_number':fake.phone_number()
+
+    } for x in range(50)]
 
 df_fake_patients = pd.DataFrame(fake_patients)
-# Drop the duplicate MRN
+# drop duplicate mrn
 df_fake_patients = df_fake_patients.drop_duplicates(subset=['mrn'])
 
-# Real ndc codes
-ndc_codes = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/FDA_NDC_CODES/main/NDC_2022_product.csv')
-ndc_codes_1k = ndc_codes.sample(n=1000, random_state=1)
-# drop duplicates from ndc_codes_1k
-ndc_codes_1k = ndc_codes_1k.drop_duplicates(subset=['PRODUCTNDC'], keep='first')
-
-# Real icd10 codes
+#### real icd10 codes
 icd10codes = pd.read_csv('https://raw.githubusercontent.com/Bobrovskiy/ICD-10-CSV/master/2020/diagnosis.csv')
 list(icd10codes.columns)
 icd10codesShort = icd10codes[['CodeWithSeparator', 'ShortDescription']]
@@ -54,48 +56,36 @@ icd10codesShort_1k = icd10codesShort.sample(n=1000)
 # drop duplicates
 icd10codesShort_1k = icd10codesShort_1k.drop_duplicates(subset=['CodeWithSeparator'], keep='first')
 
-# Real cpt codes 
-cpt_codes = pd.read_csv("https://gist.githubusercontent.com/lieldulev/439793dc3c5a6613b661c33d71fdd185/raw/25c3abcc5c24e640a0a5da1ee04198a824bf58fa/cpt4.csv")
+
+#### real ndc codes
+ndc_codes = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/FDA_NDC_CODES/main/NDC_2022_product.csv')
+ndc_codes_1k = ndc_codes.sample(n=1000, random_state=1)
+# drop duplicates from ndc_codes_1k
+ndc_codes_1k = ndc_codes_1k.drop_duplicates(subset=['PRODUCTNDC'], keep='first')
+
+#### real cpt codes
+cpt_codes = pd.read_csv('ENTER CSV')
 cpt_codes_1k = cpt_codes.sample(n=1000, random_state=1)
-# drop duplicates from cpt_codes_1k
-cpt_codes_1k = cpt_codes_1k.drop_duplicates(
-    subset = ['com.medigy.persist.reference.type.clincial.CPT.code', 'label']
-)
+cpt_codes_1k = cpt_codes.drop_duplicates(subset=['com.medigy.persist.reference.type.clincial.CPT.code'], keep='first')
 
-# From pandas to sql
-df_fake_patients.to_sql('production_patients', con=db_azure, if_exists='append', index=False)
-# query db_azure 
-df_azure = pd.read_sql_query("SELECT * FROM production_patients", db_azure)
+####real loinc codes
+loinc_codes = pd.read_csv('enter LOINC')
 
-# Insert fake medications 
-insertQuery = "INSERT INTO medications (med_ndc, med_human_name) VALUES (%s, %s)"
 
-medRowCount = 0
-for index, row in ndc_codes_1k.iterrows():
-    medRowCount += 1
-    db_azure.execute(insertQuery, (row['PRODUCTNDC'], row['NONPROPRIETARYNAME']))
+########## INSERTING IN FAKE PATIENTS ##########
+insertQuery = "INSERT INTO patients (mrn, first_name, last_name, dob, gender, zip_code, city, state, phone_number) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+
+for index, row in df_fake_patients.iterrows():
+    db_azure.execute(insertQuery, (row['mrn'], row['first_name'], row['last_name'],row['dob'], row['gender'], row['zip_code'], row['city'], row['state'], row['phone_number']))
     print("inserted row: ", index)
-    ## stop once we have 15 rows
-    if medRowCount == 15:
-        break
-# query db_azure 
-df_azure = pd.read_sql_query("SELECT * FROM production_medications", db_azure)
 
-# Inserting fake treatment_procedures 
-insertQuery = "INSERT INTO treatment_procedures (cpt_code, cpt_description) VALUES (%s, %s)"
+# # query dbs to see if data is there
+df_azure = pd.read_sql_query("SELECT * FROM patients", db_azure)
 
-starting = 0
-for index, row in cpt_codes_1k.iterrows():
-    starting += 1
-    db_azure.execute(insertQuery, (row['com.medigy.persist.reference.type.clincial.CPT.code'], row['label']))
-    print("inserted row: ", index)
-    ## stop once we have 15 rows
-    if starting == 15:
-        break
-# query db_azure 
-df_azure = pd.read_sql_query("SELECT * FROM treatment_procedures", db_azure)
 
-### insert fake conditions
+
+########## INSERTING IN CONDITIONS ##########
 insertQuery = "INSERT INTO conditions (icd10_code, icd10_description) VALUES (%s, %s)"
 
 startingRow = 0
@@ -104,75 +94,99 @@ for index, row in icd10codesShort_1k.iterrows():
     print('startingRow: ', startingRow)
     db_azure.execute(insertQuery, (row['CodeWithSeparator'], row['ShortDescription']))
     print("inserted row db_azure: ", index)
-    ## stop once we have 15 rows
-    if startingRow == 15:
+    ## stop once we have 100 rows
+    if startingRow == 100:
         break
-# query dbs 
+
+# query dbs to see if data is there
 df_azure = pd.read_sql_query("SELECT * FROM conditions", db_azure)
 
 
-# Creating fake patient medication as patient_meds
+########## INSERTING IN FAKE MEDICATIONS ##########
+insertQuery = "INSERT INTO medications (med_ndc, med_human_name) VALUES (%s, %s)"
 
-# query dbs for id
-df_medications = pd.read_sql_query("SELECT med_ndc FROM medications", db_azure) 
-df_patients = pd.read_sql_query("SELECT mrn FROM production_patients", db_azure)
+medRowCount = 0
+for index, row in ndc_codes_1k.iterrows():
+    medRowCount += 1
+    db_azure.execute(insertQuery, (row['PRODUCTNDC'], row['NONPROPRIETARYNAME']))
+    print("inserted row: ", index)
+    ## stop once we have 50 rows
+    if medRowCount == 75:
+        break
 
-# create stacked df and assign patients random number of meds between 1-5
-df_patient_medications = pd.DataFrame(columns=['mrn', 'med_ndc'])
+# query dbs to see if data is there
+df_azure = pd.read_sql_query("SELECT * FROM production_medications", db_azure)
 
-# for each patient in df_patient_medications, take a random number of medications between 1 and 10 from df_medications and palce it in df_patient_medications
+######### INSERTING IN TREATMENTS #########
+insertQuery = "INSERT INTO treatments_procedures (cpt_code, cpt_code_description) VALUES (%s, %s)"
+
+TreatmentRowCount = 0
+for index, row in cpt_codes_1k.iterrows():
+    TreatmentRowCount += 1
+    db_azure.execute(insertQuery, (row['com.medigy.persist.reference.type.clincial.CPT.code'], row['label']))
+    print("inserted row: ", index)
+    ## stop once we have 50 rows
+    if TreatmentRowCount == 75:
+        break
+
+# query dbs to see if data is there
+df_azure = pd.read_sql_query("SELECT * FROM treatments_procedures", db_azure)
+
+####### INSERTING IN SOCIAL DETERMINANTS ########
+insertQuery = "INSERT INTO social_determinants (loinc_code, loinc_code_description) VALUES (%s, %s)"
+
+RowCount = 0
+for index, row in loinc_codes.iterrows():
+    RowCount += 1
+    db_azure.execute(insertQuery, (row['loinc_code'], row['loinc_code_description']))
+    print("inserted row: ", index)
+    ## stop once we have 50 rows
+    if RowCount == 75:
+        break
+
+# query dbs to see if data is there
+df_azure = pd.read_sql_query("SELECT * FROM social_determinants", db_azure)
+
+
+######## patient_summary table ######
+
+# first, lets query conditions, medications, and patients to get the ids
+df_conditions = pd.read_sql_query("SELECT icd10_code FROM conditions", db_azure)
+df_patients = pd.read_sql_query("SELECT mrn FROM patients", db_azure)
+df_medications = pd.read_sql_query("SELECT med_ndc FROM medications", db_azure)
+
+# create a dataframe that is stacked and give each patient two conditions and two medications
+df_patient_summary = pd.DataFrame(columns=['mrn'])
 for index, row in df_patients.iterrows():
-    # get a random number of medications between 1 and 5
-    numMedications = random.randint(1, 5)
-    # get a random sample of medications from df_medications
-    df_medications_sample = df_medications.sample(n=numMedications)
+   
+    df_conditions_sample = df_conditions.sample(n=2)
+    # add the mrn to the df_conditions_sample
+    df_conditions_sample['mrn'] = row['mrn']
+ 
+    df_medications_sample = df_medications.sample(n=2)
     # add the mrn to the df_medications_sample
     df_medications_sample['mrn'] = row['mrn']
-    # append the df_medications_sample to df_patient_medications
-    df_patient_medications = df_patient_medications.append(df_medications_sample)
-print(df_patient_medications.head(15))
+    
+    df_patient_summary = df_patient_summary.append(df_conditions_sample)
+    df_patient_summary = df_patient_summary.append(df_medications_sample)
 
-# Add a random medication to each patient
-insertQuery = "INSERT INTO patient_meds (mrn, med_ndc) VALUES (%s, %s)"
+print(df_patient_summary.head(20))
 
-for index, row in df_patient_medications.iterrows():
-    db_azure.execute(insertQuery, (row['mrn'], row['med_ndc']))
+#get rid of Nan values
+df_patient_summary['icd10_code'] = df_patient_summary['icd10_code'].fillna(method='ffill').fillna(method='bfill')
+df_patient_summary['med_ndc'] = df_patient_summary['med_ndc'].fillna(method='ffill').fillna(method='bfill')
+
+print(df_patient_summary.head(20))
+
+insertQuery = "INSERT INTO patient_summary (mrn, conditions, medication) VALUES (%s, %s, %s)"
+
+for index, row in df_patient_summary.iterrows():
+    db_azure.execute(insertQuery, (row['mrn'], row['icd10_code'], row['med_ndc']))
     print("inserted row: ", index)
 
-
-### create fake patient_treatment_procedures
-
-# query dbs for id
-df_treatment_procedures = pd.read_sql_query("SELECT cpt_description FROM treatment_procedures", db_azure) 
-df_patients = pd.read_sql_query("SELECT mrn FROM production_patients", db_azure)
-
-# create stacked df and assign patients random number of meds between 1-5
-df_patient_treatment_procedures = pd.DataFrame(columns=['mrn', 'cpt_description'])
-
-# for each patient in df_patient_medications, take a random number of medications between 1 and 10 from df_medications and palce it in df_patient_medications
-for index, row in df_patients.iterrows():
-    # get a random number of medications between 1 and 5
-    numTreatment = random.randint(1, 5)
-    # get a random sample of medications from df_medications
-    df_treatment_sample = df_treatment_procedures.sample(n=numTreatment)
-    # add the mrn to the df_medications_sample
-    df_treatment_sample['mrn'] = row['mrn']
-    # append the df_medications_sample to df_patient_medications
-    df_patient_treatment_procedures = df_patient_treatment_procedures.append(df_treatment_sample)
-print(df_patient_treatment_procedures.head(15))
-
-# now lets add a random procedure to each patient
-insertQuery = "INSERT INTO patient_treatment_procedures (mrn, cpt_description) VALUES (%s, %s)"
-
-for index, row in df_patient_treatment_procedures.iterrows():
-    db_azure.execute(insertQuery, (row['mrn'], row['cpt_description']))
-    print("inserted row: ", index)
-
-
-### create fake patient_conditions
-# quesry dbs for id
+####FOR THE PATIENT CONDITIONS TABLE
 df_conditions = pd.read_sql_query("SELECT icd10_code FROM conditions", db_azure)
-df_patients = pd.read_sql_query("SELECT mrn FROM production_patients", db_azure)
+df_patients = pd.read_sql_query("SELECT mrn FROM patients", db_azure)
 
 # create a dataframe that is stacked and give each patient a random number of conditions between 1 and 5
 df_patient_conditions = pd.DataFrame(columns=['mrn', 'icd10_code'])
@@ -187,11 +201,39 @@ for index, row in df_patients.iterrows():
     df_conditions_sample['mrn'] = row['mrn']
     # append the df_conditions_sample to df_patient_conditions
     df_patient_conditions = df_patient_conditions.append(df_conditions_sample)
-print(df_patient_conditions.head(15))
+
+print(df_patient_conditions.head(20))
 
 # now lets add a random condition to each patient
 insertQuery = "INSERT INTO patient_conditions (mrn, icd10_code) VALUES (%s, %s)"
 
 for index, row in df_patient_conditions.iterrows():
     db_azure.execute(insertQuery, (row['mrn'], row['icd10_code']))
+    print("inserted row: ", index)
+
+df_medications = pd.read_sql_query("SELECT med_ndc FROM medications", db_azure) 
+df_patients = pd.read_sql_query("SELECT mrn FROM patients", db_azure)
+
+
+####FOR THE PATIENT MEDICATIONS TABLE
+# create a dataframe that is stacked and give each patient a random number of medications between 1 and 5
+df_patient_medications = pd.DataFrame(columns=['mrn', 'med_ndc'])
+# for each patient in df_patient_medications, take a random number of medications between 1 and 10 from df_medications and palce it in df_patient_medications
+for index, row in df_patients.iterrows():
+    # get a random number of medications between 1 and 5
+    numMedications = random.randint(1, 5)
+    # get a random sample of medications from df_medications
+    df_medications_sample = df_medications.sample(n=numMedications)
+    # add the mrn to the df_medications_sample
+    df_medications_sample['mrn'] = row['mrn']
+    # append the df_medications_sample to df_patient_medications
+    df_patient_medications = df_patient_medications.append(df_medications_sample)
+
+print(df_patient_medications.head(10))
+
+# now lets add a random medication to each patient
+insertQuery = "INSERT INTO patient_medications (mrn, med_ndc) VALUES (%s, %s)"
+
+for index, row in df_patient_medications.iterrows():
+    db_azure.execute(insertQuery, (row['mrn'], row['med_ndc']))
     print("inserted row: ", index)
--- sql_table_creation.py
+++ sql_table_creation.py
@@ -9,6 +9,13 @@ from faker import Faker
 import uuid
 import random
 
+def droppingFunction_all(dbList, db_source):
+    for table in dbList:
+        db_source.execute(f'drop table {table}')
+        print(f'dropped table {table} succesfully!')
+    else:
+        print(f'kept table {table}')
+
 load_dotenv()
 AZURE_MYSQL_HOSTNAME = os.getenv("AZURE_MYSQL_HOSTNAME")
 AZURE_MYSQL_USER = os.getenv("AZURE_MYSQL_USERNAME")
@@ -18,90 +25,121 @@ AZURE_MYSQL_DATABASE = os.getenv("AZURE_MYSQL_DATABASE")
 connection_string_azure = f'mysql+pymysql://{AZURE_MYSQL_USER}:{AZURE_MYSQL_PASSWORD}@{AZURE_MYSQL_HOSTNAME}:3306/{AZURE_MYSQL_DATABASE}'
 db_azure = create_engine(connection_string_azure)
 
-db_azure.table_names() 
-print(db_azure.table_names())
+#### note to self, need to ensure server_paremters => require_secure_transport is OFF in Azure 
 
-#Creating tables
-production_patients = """
-create table if not exists production_patients (
-    id int auto_increment,
-    mrn varchar(255) default null unique,
-    first_name varchar(255) default null,
-    last_name varchar(255) default null,
-    zip_code varchar(255) default null,
-    dob varchar(255) default null,
-    gender varchar(255) default null,
-    contact_mobile varchar(255) default null,
-    contact_home varchar(255) default null,
-    PRIMARY KEY (id) 
-); 
-"""
+### show tables from databases
 
-medications = """
+tableNames_azure = db_azure.table_names()
+
+# reoder tables
+tableNames_azure = ['medications','conditions', 'social_determinants','treatments_procedures','patients', 'patient_summary','patient_conditions','patient_medications']
+
+# ### delete everything 
+droppingFunction_all(tableNames_azure, db_azure)
+
+#### first step below is just creating a basic version of each of the tables,
+#### along with the primary keys and default values 
+
+table_medications = """
 create table if not exists medications (
     id int auto_increment,
     med_ndc varchar(255) default null unique,
     med_human_name varchar(255) default null,
     med_is_dangerous varchar(255) default null,
     PRIMARY KEY (id)
+    
+    
 ); 
 """
 
-treatment_procedures = """
-create table if not exists treatment_procedures (
-    id int auto_increment,
-    cpt_code varchar(255) default null unique,
-    cpt_description varchar(255) default null,
-    PRIMARY KEY (id) 
-); 
-"""
-
-conditions = """
-create table if not exists production_conditions (
+table_conditions = """
+create table if not exists conditions (
     id int auto_increment,
     icd10_code varchar(255) default null unique,
     icd10_description varchar(255) default null,
     PRIMARY KEY (id) 
+    
 ); 
 """
 
-patient_meds = """
-create table if not exists patient_medication (
+table_social_determinants = """
+create table if not exists social_determinants (
+    id int auto_increment,
+    loinc_code varchar(255) default null unique,
+    loinc_code_description varchar(255) default null,
+    PRIMARY KEY (id)
+); 
+"""
+
+table_treatments_procedures = """
+create table if not exists treatments_procedures (
+    id int auto_increment,
+    cpt_code varchar(255) default null unique,
+    cpt_code_description varchar(255) default null,
+    PRIMARY KEY (id)
+    
+); 
+"""
+
+table_patients = """
+create table if not exists patients (
+    id int auto_increment,
+    mrn varchar(255) default null unique,
+    first_name varchar(255) default null,
+    last_name varchar(255) default null,
+    dob varchar(255) default null,
+    gender varchar(255) default null,
+    city varchar(255) default null,
+    state varchar(255) default null,
+    phone_number varchar(255) default null,
+    zip_code varchar(255) default null
+    PRIMARY KEY (id)   
+); """
+
+table_patient_summary = """
+create table if not exists patient_summary (
     id int auto_increment,
     mrn varchar(255) default null,
-    med_ndc varchar(255) default null,
+    conditions varchar(255) default null,
+    medication varchar(255) default null,
     PRIMARY KEY (id),
-    FOREIGN KEY (mrn) REFERENCES production_patients(mrn) ON DELETE CASCADE,
-    FOREIGN KEY (med_ndc) REFERENCES medications(med_ndc) ON DELETE CASCADE
+    FOREIGN KEY (mrn) REFERENCES patients(mrn) ON DELETE CASCADE,
+    FOREIGN KEY (conditions) REFERENCES conditions(icd10_code) ON DELETE CASCADE,
+    FOREIGN KEY (medication) REFERENCES medications(med_ndc) ON DELETE CASCADE
 ); 
 """
 
-patient_treatment_procedures = """
-create table if not exists patient_treatment_procedures (
-    id int auto_increment,
-    mrn varchar(255) default null,
-    cpt_code varchar(255) default null,
-    PRIMARY KEY (id),
-    FOREIGN KEY (mrn) REFERENCES production_patients(mrn) ON DELETE CASCADE,
-    FOREIGN KEY (cpt_code) REFERENCES treatment_procedures(cpt_code) ON DELETE CASCADE
-); 
-"""
-
-patient_conditions = """
+table_prod_patient_conditions = """
 create table if not exists patient_conditions (
     id int auto_increment,
     mrn varchar(255) default null,
     icd10_code varchar(255) default null,
     PRIMARY KEY (id),
-    FOREIGN KEY (mrn) REFERENCES production_patients(mrn) ON DELETE CASCADE,
+    FOREIGN KEY (mrn) REFERENCES patients(mrn) ON DELETE CASCADE,
     FOREIGN KEY (icd10_code) REFERENCES conditions(icd10_code) ON DELETE CASCADE
 ); 
 """
 
-db_azure.execute(production_patients)
-db_azure.execute(medications)
-db_azure.execute(treatment_procedures)
-db_azure.execute(conditions)
-db_azure.execute(patient_meds)
-db_azure.execute(patient_treatment_procedures)
-db_azure.execute(patient_conditions)
+table_prod_patients_medications = """
+create table if not exists patient_medications (
+    id int auto_increment,
+    mrn varchar(255) default null,
+    med_ndc varchar(255) default null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (mrn) REFERENCES patients(mrn) ON DELETE CASCADE,
+    FOREIGN KEY (med_ndc) REFERENCES medications(med_ndc) ON DELETE CASCADE
+); 
+"""
+
+#execute tables
+db_azure.execute(table_patients)
+db_azure.execute(table_medications)
+db_azure.execute(table_conditions)
+db_azure.execute(table_treatments_procedures)
+db_azure.execute(table_social_determinants)
+db_azure.execute(table_patient_summary)
+db_azure.execute(table_prod_patient_conditions)
+db_azure.execute(table_prod_patients_medications)
+
+# get tables from db_azure
+azure_tables = db_azure.table_names()
